c90cb1b09938cbe9973f5156f23f65693c2e0e0f
wandatrijhuliana <wandatjh2003@gmail.com>
2019-10-15 15:47:50 +0700

    first commit

---
+++ app/src/main/java/com/example/Praktik3/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.Praktik3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mShowCount = findViewById(R.id.show_count);
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void showToast(View view) {
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context,"Hello Toast",Toast.LENGTH_LONG);
+        toast.show();
+    }
+}
